4547f843ef1b2b1138579353d6ea42e97f96cfdc
Cameron-Witcher <admin@hyverse.network>
2020-02-27 09:18:19 -0600

    okok

--- src/main/java/net/mysticcloud/spigot/core/utils/CoreUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/CoreUtils.java
@@ -124,8 +124,8 @@ public class CoreUtils {
 
 		if (testSQLConnection()) {
 			connected = true;
-			db = new IDatabase(SQLDriver.MYSQL, "localhost", "Minecraft", 3306, "root", "v4pob8LW");
-			wbconn = new IDatabase(SQLDriver.MYSQL, "localhost", "Website", 3306, "root", "v4pob8LW");
+			db = new IDatabase(SQLDriver.MYSQL, "localhost", "Minecraft", 3306, "mysql", "v4pob8LW");
+			wbconn = new IDatabase(SQLDriver.MYSQL, "localhost", "Website", 3306, "mysql", "v4pob8LW");
 			Bukkit.getConsoleSender().sendMessage(prefixes.get("sql") + "Successfully connected to MySQL.");
 		} else {
 			connected = false;
