3d674a4efa051b27e6912b2f4eb50779dbbf4168
Cameron-Witcher <CameronWitcher1@gmail.com>
2020-04-02 19:18:43 -0500

    Hopefully that'll keep the player from joining

--- src/main/java/net/mysticcloud/spigot/core/listeners/PlayerListener.java
+++ src/main/java/net/mysticcloud/spigot/core/listeners/PlayerListener.java
@@ -105,8 +105,16 @@ public class PlayerListener implements Listener {
 		for (Punishment punish : PunishmentUtils.getPunishments()) {
 			if (punish.getUser().equals(e.getPlayer().getUniqueId().toString())) {
 				if (punish.getType().equals(PunishmentType.BAN)) {
-					e.getPlayer().kickPlayer(CoreUtils.colorize("&cYou're are banned for " + CoreUtils
-							.getSimpleTimeFormat(punish.getDate() + punish.getDuration() - new Date().getTime()) + "\n&f[Reason] " + punish.getNotes()));
+					Bukkit.getScheduler().runTaskLater(Main.getPlugin(), new Runnable(){
+
+						@Override
+						public void run() {
+							e.getPlayer().kickPlayer(CoreUtils.colorize("&cYou're are banned for " + CoreUtils
+									.getSimpleTimeFormat(punish.getDate() + punish.getDuration() - new Date().getTime()) + "\n&f[Reason] " + punish.getNotes()));
+						}
+						
+					}, 10);
+					
 				}
 			}
 		}
