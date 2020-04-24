4e64ebf2e3b7c8567b05aa1eb50c4a4b65a66098
Cameron-Witcher <CameronWitcher1@gmail.com>
2020-04-24 18:20:26 -0500

    Band #2?

--- src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/RainbowFormat.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/RainbowFormat.java
@@ -3,8 +3,10 @@ package net.mysticcloud.spigot.core.utils.particles.formats;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.Particle.DustOptions;
 import org.bukkit.inventory.ItemStack;
 
 import net.mysticcloud.spigot.core.utils.CoreUtils;
@@ -15,17 +17,29 @@ public class RainbowFormat extends ParticleFormat {
 	public RainbowFormat() {
 		name = "Rainbow";
 		guiItem = new ItemStack(Material.COAL);
-		particle = Particle.COMPOSTER;
+		particle = Particle.REDSTONE;
 	}
 
 	@Override
 	public void display(UUID uid, int i) {
-		if(particle == null) return;
-		for (int a = 1; a != 41; a++) {
-					spawnParticle(uid, particle,
-							Bukkit.getPlayer(uid).getLocation().clone().add(
-									0, Math.cos(Math.toRadians(a) * ((360) / (40))) * (1),
-									Math.sin(Math.toRadians(a) * ((360) / (40))) * (1)));
+
+		if (particle == null)
+			return;
+		setDustOptions(new DustOptions(Color.RED, 1));
+		for (int a = 1; a != 21; a++) {
+			spawnParticle(uid, particle,
+					Bukkit.getPlayer(uid).getLocation().clone().add(0,
+							2 + Math.cos(Math.toRadians(a) * ((360) / (40))) * (1),
+							Math.sin(Math.toRadians(a) * ((360) / (40))) * (1)));
+
+		}
+		
+		setDustOptions(new DustOptions(Color.ORANGE, 1));
+		for (int a = 1; a != 21; a++) {
+			spawnParticle(uid, particle,
+					Bukkit.getPlayer(uid).getLocation().clone().add(0,
+							2 + Math.cos(Math.toRadians(a) * ((360) / (40))) * (0.8),
+							Math.sin(Math.toRadians(a) * ((360) / (40))) * (0.8)));
 
 		}
 
