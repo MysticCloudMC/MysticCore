4fe6cd7610bdbb2da9a746d2817b1b3c29fe1c4e
Cameron-Witcher <CameronWitcher1@gmail.com>
2020-04-24 12:59:41 -0500

    bn

--- src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/CapeFormat.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/CapeFormat.java
@@ -17,47 +17,47 @@ public class CapeFormat extends ParticleFormat {
 		name = "Cape";
 		guiItem = new ItemStack(Material.RED_BANNER);
 		particle = Particle.CRIT;
-		
+
 	}
 
 	@Override
 	public void display(UUID uid, int i) {
 		super.display(uid, i);
-		if(particle == null) return;
+		if (particle == null)
+			return;
 		Location loc = Bukkit.getPlayer(uid).getLocation().clone();
-		
-		 double r = 2;
-		 double t = Bukkit.getPlayer(uid).getEyeLocation().getYaw();
-//         double x = r * Math.cos(t);
-		 double y = Bukkit.getPlayer(uid).getEyeLocation().getY();
-//         double z = r * Math.sin(t);
-         double x = Bukkit.getPlayer(uid).getEyeLocation().getX();
-         double z = Bukkit.getPlayer(uid).getEyeLocation().getZ();
-         Vector v = new Vector(x, y, z);
-         v = rotateAroundAxisY(v, t);
-         loc.add(v.getX(), v.getY(), v.getZ());
-         
-         spawnParticle(uid, particle, Bukkit.getPlayer(uid).getLocation().clone().add(
- 				2,0,0),0,-0.5,0);
 
-		
+		double r = 2;
+		double t = Bukkit.getPlayer(uid).getEyeLocation().getYaw();
+		// double x = r * Math.cos(t);
+		double y = Bukkit.getPlayer(uid).getEyeLocation().getY();
+		// double z = r * Math.sin(t);
+		double x = Bukkit.getPlayer(uid).getEyeLocation().getX();
+		double z = Bukkit.getPlayer(uid).getEyeLocation().getZ();
+		Vector v = new Vector(x, y, z);
+		v = rotateAroundAxisY(v, t);
+		loc.add(2, 0, 0);
+		loc.add(v.getX(), v.getY(), v.getZ());
+
+		spawnParticle(uid, particle, loc, 0, -0.5, 0);
+
 	}
-	
+
 	private Vector rotateAroundAxisY(Vector v, double cos, double sin) {
-        double x = v.getX() * cos + v.getZ() * sin;
-        double z = v.getX() * -sin + v.getZ() * cos;
-        return v.setX(x).setZ(z);
-    }
-	
+		double x = v.getX() * cos + v.getZ() * sin;
+		double z = v.getX() * -sin + v.getZ() * cos;
+		return v.setX(x).setZ(z);
+	}
+
 	private Vector rotateAroundAxisY(Vector v, double angle) {
-        angle = -angle;
-        angle = Math.toRadians(angle);
-        double x, z, cos, sin;
-        cos = Math.cos(angle);
-        sin = Math.sin(angle);
-        x = v.getX() * cos + v.getZ() * sin;
-        z = v.getX() * -sin + v.getZ() * cos;
-        return v.setX(x).setZ(z);
-    }
+		angle = -angle;
+		angle = Math.toRadians(angle);
+		double x, z, cos, sin;
+		cos = Math.cos(angle);
+		sin = Math.sin(angle);
+		x = v.getX() * cos + v.getZ() * sin;
+		z = v.getX() * -sin + v.getZ() * cos;
+		return v.setX(x).setZ(z);
+	}
 
 }
