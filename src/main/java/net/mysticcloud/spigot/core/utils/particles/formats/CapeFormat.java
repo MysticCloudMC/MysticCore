03d463bbc9d48363a769886ac6acdeadb18a8bc5
Cameron-Witcher <CameronWitcher1@gmail.com>
2020-04-24 13:14:45 -0500

    Restart lol

--- src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/CapeFormat.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/CapeFormat.java
@@ -9,10 +9,14 @@ import org.bukkit.Particle;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import net.mysticcloud.spigot.core.utils.CoreUtils;
 import net.mysticcloud.spigot.core.utils.particles.ParticleFormat;
 
 public class CapeFormat extends ParticleFormat {
 
+	
+	double t = 0;
+	
 	public CapeFormat() {
 		name = "Cape";
 		guiItem = new ItemStack(Material.RED_BANNER);
@@ -25,21 +29,43 @@ public class CapeFormat extends ParticleFormat {
 		super.display(uid, i);
 		if (particle == null)
 			return;
+		
+
+//		double r = 2;
+//		double t = Bukkit.getPlayer(uid).getEyeLocation().getYaw();
+//		// double x = r * Math.cos(t);
+//		double y = Bukkit.getPlayer(uid).getEyeLocation().getY();
+//		// double z = r * Math.sin(t);
+//		double x = Bukkit.getPlayer(uid).getEyeLocation().getX()+2;
+//		double z = Bukkit.getPlayer(uid).getEyeLocation().getZ();
+//		Vector v = new Vector(x, y, z);
+//		v = rotateAroundAxisY(v, t);
+////		loc.add(2, 0, 0);
+//		loc.add(v.getX(), v.getY(), v.getZ());
+//
+//		spawnParticle(uid, particle, loc, 0, -0.5, 0);
+		
+		
 		Location loc = Bukkit.getPlayer(uid).getLocation().clone();
 
-		double r = 2;
-		double t = Bukkit.getPlayer(uid).getEyeLocation().getYaw();
-		// double x = r * Math.cos(t);
-		double y = Bukkit.getPlayer(uid).getEyeLocation().getY();
-		// double z = r * Math.sin(t);
-		double x = Bukkit.getPlayer(uid).getEyeLocation().getX();
-		double z = Bukkit.getPlayer(uid).getEyeLocation().getZ();
-		Vector v = new Vector(x, y, z);
-		v = rotateAroundAxisY(v, t);
-		loc.add(2, 0, 0);
-		loc.add(v.getX(), v.getY(), v.getZ());
+            t = t + Math.PI / 16;
+            double x = 2 * Math.cos(t);
+            double y = 0;
+            double z = 2 * Math.sin(t);
+            Vector v = new Vector(x, 0, z);
+            v = rotateAroundAxisY(v, 10);
+            loc.add(v.getX(), v.getY(), v.getZ());
+            
+            spawnParticle(uid,particle, loc, 0, 0, 0);
 
-		spawnParticle(uid, particle, loc, 0, -0.5, 0);
+//            ParticleEffect.FIREWORKS_SPARK
+//                    .display(0, 0, 0, 0, 1, loc, 100D);
+            loc.subtract(v.getX(), v.getY(), v.getZ());
+            if (t > Math.PI * 8) {
+                t = 0;
+                CoreUtils.debug("particle reset");
+            }
+        
 
 	}
 
