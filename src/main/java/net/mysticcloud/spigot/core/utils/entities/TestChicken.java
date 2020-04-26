ae8c4979ca96febffd2bdc72b7cf0b1d67310e37
Cameron-Witcher <CameronWitcher1@gmail.com>
2020-04-26 17:34:37 -0500

    so this should actually allow custom entities to be summoned

--- src/main/java/net/mysticcloud/spigot/core/utils/entities/TestChicken.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/entities/TestChicken.java
@@ -2,18 +2,24 @@ package net.mysticcloud.spigot.core.utils.entities;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
 import net.minecraft.server.v1_15_R1.EntityChicken;
 import net.minecraft.server.v1_15_R1.EntityTypes;
+import net.minecraft.server.v1_15_R1.World;
 
 public class TestChicken extends EntityChicken {
 
-	public TestChicken(org.bukkit.World world) {
-        // I like to use the bukkit world since that makes thing easier
-        super(EntityTypes.CHICKEN, ((CraftWorld) world).getHandle());
-    }
+	public TestChicken(World world, EntityTypes<? extends EntityChicken> entityType) {
+	    this(world);
+	}
+	public TestChicken(EntityTypes<? extends EntityChicken> entityType, World world) {
+	    this(world);
+	}
+
+	public TestChicken(World world) {
+	    super(EntityTypes.CHICKEN, world);
+	}
 
 	public void spawn(Location loc) {
 		Bukkit.broadcastMessage("CustomChicken spawned!");
