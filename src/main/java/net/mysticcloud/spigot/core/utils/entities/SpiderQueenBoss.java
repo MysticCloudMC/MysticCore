03eb5b23f0bf3c2249ae2039cc36fb2520fbabbd
Owner <Owner@My-PC>
2020-05-23 13:50:15 -0500

    Maker 'er look purdy

--- src/main/java/net/mysticcloud/spigot/core/utils/entities/SpiderQueenBoss.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/entities/SpiderQueenBoss.java
@@ -1,16 +1,21 @@
 package net.mysticcloud.spigot.core.utils.entities;
 
+import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Particle.DustOptions;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
 import net.minecraft.server.v1_15_R1.DamageSource;
 import net.minecraft.server.v1_15_R1.EntitySpider;
 import net.minecraft.server.v1_15_R1.EntityTypes;
 import net.minecraft.server.v1_15_R1.World;
+import net.mysticcloud.spigot.core.utils.particles.formats.CircleFeetFormat;
 
 public class SpiderQueenBoss extends EntitySpider {
 
 	int z = 0;
+	private CircleFeetFormat format = new CircleFeetFormat();
 
 
 	public SpiderQueenBoss(World world, EntityTypes<? extends EntitySpider> entityType) {
@@ -28,6 +33,11 @@ public class SpiderQueenBoss extends EntitySpider {
 	public void spawn(Location loc) {
 		this.setPositionRotation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
 		this.world.addEntity(this, CreatureSpawnEvent.SpawnReason.CUSTOM);
+		format.setHeight(1);
+		format.setRadius(1.25);
+		format.setSpots(20);
+		format.particle(Particle.REDSTONE);
+		format.setDustOptions(new DustOptions(Color.WHITE, 1));
 	}
 
 	@Override
@@ -40,10 +50,10 @@ public class SpiderQueenBoss extends EntitySpider {
 		super.movementTick();
 		
 		
-		if(z%1000 == 0) {
+		if(z%500 == 0) {
 			new SpiderQueenMinion(world).spawn(getBukkitEntity().getLocation());
 		}
-		
+		format.display(getBukkitEntity().getLocation(), z);
 		z = z + 1;
 		
 	}
