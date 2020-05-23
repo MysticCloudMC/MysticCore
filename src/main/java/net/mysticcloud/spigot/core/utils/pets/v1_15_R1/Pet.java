746ed48baa0f6727e81e92f6b938e0aeda256f88
Owner <Owner@My-PC>
2020-05-22 23:56:00 -0500

    wow

--- src/main/java/net/mysticcloud/spigot/core/utils/pets/v1_15_R1/Pet.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/pets/v1_15_R1/Pet.java
@@ -96,8 +96,8 @@ public class Pet extends EntityArmorStand {
 		
 		float A = (float) (Math.sqrt(Math.pow(Bukkit.getPlayer(owner).getLocation().getX() - (locX()), 2)));
 		float O = (float) (Math.sqrt(Math.pow(Bukkit.getPlayer(owner).getLocation().getZ() - (locZ()), 2)));
-		CoreUtils.debug((Math.tan(45)));
-		this.yaw =  (float) (360-Math.toDegrees(Math.tan(O/A)));
+		CoreUtils.debug(Math.toDegrees(Math.atan(45)));
+		this.yaw =  (float) (Math.toDegrees(Math.atan(O/A)));
 //		if ((new Location(getWorld().getWorld(), locX() + 1, locY(), locZ()).getBlock().getType() != Material.AIR)
 //				|| (new Location(getWorld().getWorld(), locX(), locY(), locZ() + 1).getBlock()
 //						.getRelative(BlockFace.DOWN).getType() != Material.AIR)
