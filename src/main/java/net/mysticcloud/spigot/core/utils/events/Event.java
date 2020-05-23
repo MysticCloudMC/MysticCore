65236469fb9f7b43317319ada54472f7e3cb69e1
Owner <Owner@My-PC>
2020-05-23 18:33:21 -0500

    metadataoverride

--- src/main/java/net/mysticcloud/spigot/core/utils/events/Event.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/events/Event.java
@@ -94,11 +94,16 @@ public class Event {
 	}
 
 	public void setMetadata(String key, Object value) {
-//		while (true) {
-//			if (metadata.containsKey(key))
-//				key = key + "_1";
-//			break;
-//		}
+		while (true) {
+			if (metadata.containsKey(key))
+				key = key + "_1";
+			break;
+		}
+
+		metadata.put(key, value);
+	}
+	public void overrideMetadata(String key, Object value) {
+		
 
 		metadata.put(key, value);
 	}
--- src/main/java/net/mysticcloud/spigot/core/utils/events/EventUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/events/EventUtils.java
@@ -143,7 +143,7 @@ public class EventUtils {
 					}
 					CoreUtils.debug(((LivingEntity)bos).getHealth() * ( 100 / ( (LivingEntity) bos ).getMaxHealth() ) );
 					((BossBar)e.getMetadata("BOSSBAR")).setProgress((double) (((LivingEntity)bos).getHealth() * ( 100 / ( (LivingEntity) bos ).getMaxHealth() )/100));
-					e.setMetadata("LOCATION", Bukkit.getEntity((UUID) e.getMetadata("UUID")).getLocation());
+					e.overrideMetadata("LOCATION", Bukkit.getEntity((UUID) e.getMetadata("UUID")).getLocation());
 				}
 				if (e.getEventType().equals(EventType.TIMED))
 					return CoreUtils.getDate().getTime()
