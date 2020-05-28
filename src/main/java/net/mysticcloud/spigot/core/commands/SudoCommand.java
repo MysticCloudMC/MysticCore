c1087e3dcf206437df2cfbd7c9598f8c65c626ad
Owner <Owner@My-PC>
2020-05-28 15:39:41 -0500

    lol

--- src/main/java/net/mysticcloud/spigot/core/commands/SudoCommand.java
+++ src/main/java/net/mysticcloud/spigot/core/commands/SudoCommand.java
@@ -10,27 +10,36 @@ import net.mysticcloud.spigot.core.utils.CoreUtils;
 
 public class SudoCommand implements CommandExecutor {
 
-	public SudoCommand(Main plugin, String cmd){
+	public SudoCommand(Main plugin, String cmd) {
 		plugin.getCommand(cmd).setExecutor(this);
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(sender.hasPermission("mysticcloud.admin.cmd.sudo")){
-			if(args.length > 1) {
-				if(Bukkit.getPlayer(args[0]) == null) {
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender.hasPermission("mysticcloud.admin.cmd.sudo")) {
+			if (args.length > 1) {
+				if (Bukkit.getPlayer(args[0]) == null) {
 					sender.sendMessage(CoreUtils.prefixes("admin") + "Player not online.");
 					return true;
 				}
 				String command = "";
-				for(int s=1;s!=args.length;s++) {
+				for (int s = 1; s != args.length; s++) {
 					command = command == "" ? args[s] : command + " " + args[s];
 				}
-				if(args[1].startsWith("/")) {
+				if (args[1].startsWith("/")) {
 					command = command.replaceFirst("/", "");
 					Bukkit.getPlayer(args[0]).performCommand(command);
-				} else {
-					Bukkit.getPlayer(args[0]).chat(command);
+					return true;
 				}
+				if (args[1].startsWith("*walk")) {
+					command = command.replaceFirst("*walk", "");
+					String[] loc = command.split(" ");
+					Bukkit.getPlayer(args[0]).teleport(Bukkit.getPlayer(args[0]).getLocation()
+							.add(Integer.parseInt(loc[0]), Integer.parseInt(loc[1]), Integer.parseInt(loc[2])));
+					return true;
+				}
+
+				Bukkit.getPlayer(args[0]).chat(command);
+
 			} else {
 				sender.sendMessage(CoreUtils.prefixes("admin") + "/sudo <user> <command>");
 			}
