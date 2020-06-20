0cea7c17e5e098ed130d1d2967f66f3c114db69b
Owner <Owner@My-PC>
2020-06-20 01:52:55 -0500

    Made seen command much more stalker-y

--- src/main/java/net/mysticcloud/spigot/core/commands/SeenCommand.java
+++ src/main/java/net/mysticcloud/spigot/core/commands/SeenCommand.java
@@ -1,5 +1,7 @@
 package net.mysticcloud.spigot.core.commands;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -24,8 +26,39 @@ public class SeenCommand implements CommandExecutor {
 				if(uid != null){
 					SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd yyyy hh:mm");
 					
-					String seen = sdf.format(new Date(CoreUtils.LookupLastSeen(uid)));
-					sender.sendMessage(CoreUtils.colorize("&3&lMysticCore &7>&f " + args[0] + " was last seen: " + (seen)));
+					String server = "NaN";
+					String ip = "NaN";
+					long date = 0;
+					
+					ResultSet iprs = CoreUtils.sendQuery("SELECT IP,UUID FROM PlayerStats");
+					try {
+						while (iprs.next()) {
+							if (iprs.getString("UUID").equalsIgnoreCase(uid.toString())) {
+								ip = iprs.getString("IP");
+								date = Long.parseLong(iprs.getString("DATE"));
+								break;
+							}
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					
+					ResultSet srvrs = CoreUtils.sendQuery("SELECT SERVER FROM ServerStats WHERE UUID='" + uid + "' ORDER BY DATE DESC");
+					try {
+						while (srvrs.next()) {
+							if (!srvrs.getString("SERVER").equalsIgnoreCase("NaN")) {
+								server = srvrs.getString("SERVER");
+								break;
+							}
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+					String seen = sdf.format(new Date(date));
+					
+					
+					sender.sendMessage(CoreUtils.colorize("&3&lMysticCore &7>&f " + args[0] + " was last seen &7" + (seen) + "&f, on &7" + server + "&f, with &7" + ip + "&f."));
 					
 				} else {
 					sender.sendMessage(CoreUtils.colorize("&3&lMysticCore &7>&f That player couldn't be found in the database."));
