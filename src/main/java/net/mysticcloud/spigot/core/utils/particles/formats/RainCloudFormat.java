8acb0d98266c05ed00251805b87cb3d2229408c7
Owner <Owner@My-PC>
2020-09-22 20:27:22 -0500

    lets change how we think about this

--- src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/RainCloudFormat.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/particles/formats/RainCloudFormat.java
@@ -46,23 +46,16 @@ public class RainCloudFormat extends ParticleFormat {
 					if (0.05 > CoreUtils.getRandom().nextDouble() * 100) {
 
 //						for (int f = 0; f != 2; f++) {
-							LinkedList<Vector> points = new LinkedList<>();
-							points.add(new Vector(loc.getX(), loc.getY(), loc.getZ()));
-							for (int z = 0; z != 5 * scalar; i++) {
-								Vector point = points.get(points.size() - 1).clone();
-								point.add(loc.add(0, (double) -(1 / scalar), 0).toVector());
-								point.setX(point.getX() + (CoreUtils.getRandom().nextDouble()
-										* (CoreUtils.getRandom().nextBoolean() ? (double) 1 / scalar
-												: -(double) 1 / scalar)));
-								point.setY(point.getY() + (CoreUtils.getRandom().nextDouble()
-										* (CoreUtils.getRandom().nextBoolean() ? (double) 1 / scalar
-												: -(double) 1 / scalar)));
-								point.setZ(point.getZ() + (CoreUtils.getRandom().nextDouble()
-										* (CoreUtils.getRandom().nextBoolean() ? (double) 1 / scalar
-												: -(double) 1 / scalar)));
-								spawnParticle(Particle.END_ROD,
-										new Location(loc.getWorld(), point.getX(), point.getY(), point.getZ()));
-							}
+						for (int z = 0; z != 5 * scalar; i++) {
+							loc.add((CoreUtils.getRandom().nextDouble()
+									* (CoreUtils.getRandom().nextBoolean() ? (double) 1 / scalar
+											: -(double) 1 / scalar)),
+									-1 / scalar,
+									(CoreUtils.getRandom().nextDouble()
+											* (CoreUtils.getRandom().nextBoolean() ? (double) 1 / scalar
+													: -(double) 1 / scalar)));
+							spawnParticle(Particle.END_ROD, loc);
+						}
 //							for (Vector vec : points) {
 //								Location loca = new Location(loc.getWorld(), vec.getX(), vec.getY(), vec.getZ());
 //								spawnParticle(Particle.END_ROD, loca);
