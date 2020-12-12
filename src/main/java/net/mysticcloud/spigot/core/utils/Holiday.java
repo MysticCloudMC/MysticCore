d33981806cf917f5d4b8dfb7fcec1055a53b1119
Camer <Camer@DESKTOP-OHRRTRL>
2020-12-11 21:24:03 -0600

    Moved holiday desc to Holiday class
    They were in the PLACEHOLDER class! What was I thinking!!

--- src/main/java/net/mysticcloud/spigot/core/utils/Holiday.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/Holiday.java
@@ -4,24 +4,29 @@ import org.bukkit.Material;
 
 public enum Holiday {
 	
-	CHRISTMAS("Christmas"),
-	NEW_YEARS("New Years"),
-	HALLOWEEN("Halloweed"), 
+	CHRISTMAS("Christmas","&c&lMerry &a&lChristmas&f"),
+	NEW_YEARS("New Years","&a&lHappy New Years!"),
+	HALLOWEEN("Halloween","&6&lHappy &5&lHalloween&f"), 
 	NONE(""), 
 	VALENTINES("Valentines Day"), 
 	BIRTHDAY("Birthday"), 
-	ST_PATRICKS("St. Patrick's Day"), 
-	CINCO_DE_MAYO("Cinco de Mayo"), 
-	AVACADO_DAY("(USA) National Avacado Day"), 
+	ST_PATRICKS("St. Patrick's Day","&fHappy &a&lSt. Patrick's day&f"), 
+	CINCO_DE_MAYO("Cinco de Mayo","&c&lCinco &f&lDe &a&lMayo"), 
+	AVACADO_DAY("(USA) National Avacado Day", "&2&lAVACADO DAY!"), 
 	TEST("Test Holiday"),
-	MAY_THE_FORTH("May The Forth"),
-	JULY_4TH("Forth of July (USA)"),
-	MEMORIAL_DAY("Memorial Day (USA)");
+	MAY_THE_FORTH("May The Forth","&eMay The Forth Be With You"),
+	JULY_4TH("Forth of July (USA)","&fKeep your fingers safe!"),
+	MEMORIAL_DAY("Memorial Day (USA)","&cThank &fYour &9Veterans");
 	
 	String name;
+	String line = "";
 	
 	Holiday(String name){
 		this.name = name;
+	}
+	Holiday(String name,String line){
+		this.name = name;
+		this.line = line;
 	}
 	
 	public String getName() {
@@ -31,6 +36,10 @@ public enum Holiday {
 	Material getGUIItem() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public String getScoreboardLine() {
+		return line;
 	}
 
 }
--- src/main/java/net/mysticcloud/spigot/core/utils/placeholder/PlaceholderUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/placeholder/PlaceholderUtils.java
@@ -7,57 +7,27 @@ import net.mysticcloud.spigot.core.utils.Holiday;
 import net.mysticcloud.spigot.core.utils.MysticPlayer;
 
 public class PlaceholderUtils {
-	
-	
+
 	public static String replace(Player player, String string) {
 		MysticPlayer mp = CoreUtils.getMysticPlayer(player);
 		string = string.replaceAll("%player", player.getName());
 		string = string.replaceAll("%pl", player.getName());
 		string = string.replaceAll("%world", player.getWorld().getName());
 		string = string.replaceAll("%time", CoreUtils.getTime());
-		string = string.replaceAll("%balance", ""+mp.getBalance());
-		string = string.replaceAll("%gems", ""+mp.getGems());
-		string = string.replaceAll("%g", ""+mp.getGems());
-		string = string.replaceAll("%level", ""+mp.getLevel());
-		string = string.replaceAll("%rank", CoreUtils.colorize(""+CoreUtils.getPlayerPrefix(player)));
-		string = string.replaceAll("%prefix", CoreUtils.colorize(""+CoreUtils.getPlayerPrefix(player)));
-		string = string.replaceAll("%r", CoreUtils.colorize(""+CoreUtils.getPlayerPrefix(player)));
-		
-		if(!CoreUtils.getHoliday().equals(Holiday.NONE)) {
-			switch(CoreUtils.getHoliday()) {
-			case CHRISTMAS:
-				string = string.replaceAll("%hdayline", "&c&lMerry &a&lChristmas&f");
-				break;
-			case TEST:
-				string = string.replaceAll("%hdayline", "&fHappy &a&lSt. Patrick's day&f");
-				break;
-			case AVACADO_DAY:
-				string = string.replaceAll("%hdayline", "&2&lAVACADO DAY!");
-				break;
-			case BIRTHDAY:
-				string = string.replaceAll("%hdayline", "&bHappy Birthday!");
-				break;
-			case HALLOWEEN:
-				string = string.replaceAll("%hdayline", "&6&lHappy &5&lHolloween&f");
-			case CINCO_DE_MAYO:
-				string = string.replaceAll("%hdayline", "&c&lCinco &f&lDe &a&lMayo");
-				break;
-			case MAY_THE_FORTH:
-				string = string.replaceAll("%hdayline", "&eMay The Forth Be With You");
-				break;
-			case MEMORIAL_DAY:
-				string = string.replaceAll("%hdayline", "&cThank &fYour &bVeterans");
-				break;
-			default:
-				break;
-			}
+		string = string.replaceAll("%balance", "" + mp.getBalance());
+		string = string.replaceAll("%gems", "" + mp.getGems());
+		string = string.replaceAll("%g", "" + mp.getGems());
+		string = string.replaceAll("%level", "" + mp.getLevel());
+		string = string.replaceAll("%rank", CoreUtils.colorize("" + CoreUtils.getPlayerPrefix(player)));
+		string = string.replaceAll("%prefix", CoreUtils.colorize("" + CoreUtils.getPlayerPrefix(player)));
+		string = string.replaceAll("%r", CoreUtils.colorize("" + CoreUtils.getPlayerPrefix(player)));
+		if (!CoreUtils.getHoliday().equals(Holiday.NONE)) {
 			string = string.replaceAll("%holiday", "&b" + CoreUtils.getHoliday().getName());
-			
+			string = string.replaceAll("%hdayline", CoreUtils.getHoliday().getScoreboardLine());
 		} else {
 			string = string.replaceAll("%holiday", "");
 			string = string.replaceAll("%hdayline", "");
 		}
-		
 		return string;
 	}
 
