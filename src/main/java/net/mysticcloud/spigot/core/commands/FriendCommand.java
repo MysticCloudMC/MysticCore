17d54d8660c74cb1187812b8694ffcfa44a9a98d
Camer <Camer@DESKTOP-OHRRTRL>
2020-12-20 11:37:43 -0600

    Lets go

--- src/main/java/net/mysticcloud/spigot/core/commands/FriendCommand.java
+++ src/main/java/net/mysticcloud/spigot/core/commands/FriendCommand.java
@@ -18,20 +18,40 @@ public class FriendCommand implements CommandExecutor {
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("friends")) {
-			List<String> friendsl = CoreUtils.getMysticPlayer((Player)sender).getFriends();
-			String friends = "";
-			for(String s : friendsl) {
-				friends = friends == "" ? s : CoreUtils.LookupForumID(s) + ", " + s;
+		if (sender instanceof Player) {
+
+			if (cmd.getName().equalsIgnoreCase("friends")) {
+				List<String> friendsl = CoreUtils.getMysticPlayer((Player) sender).getFriends();
+				String friends = "";
+				for (String s : friendsl) {
+					friends = friends == "" ? s : CoreUtils.LookupForumID(s) + ", " + s;
+				}
+				sender.sendMessage(CoreUtils.prefixes("friends") + "Below is a list of your friends:");
+				sender.sendMessage(CoreUtils.colorize("&f" + (friends == ""
+						? "&cYou don't have any registered friends. &fTo add a friend link your Minecraft account with your MysticCloud web account, and friend a user that has done the same!"
+						: friends)));
 			}
-			sender.sendMessage(CoreUtils.prefixes("friends") + "Below is a list of your friends:");
-			sender.sendMessage(CoreUtils.colorize("&f" + (friends == "" ? "&cYou don't have any registered friends. &fTo add a friend link your Minecraft account with your MysticCloud web account, and friend a user that has done the same!" : friends)));
-		}
-		if (cmd.getName().equalsIgnoreCase("friend")) {
-			
-//			sender.sendMessage("" + CoreUtils.LookupForumID(args[0]));
-			
-			sender.sendMessage("" +CoreUtils.getMysticPlayer((Player)sender).isFriends(args[0]));
+			if (cmd.getName().equalsIgnoreCase("friend")) {
+				if (CoreUtils.LookupUUID(args[0]) != null) {
+					if (CoreUtils.getMysticPlayer((Player) sender).isFriends(args[0])) {
+						sender.sendMessage(CoreUtils.prefixes("friends") + CoreUtils
+								.colorize("When this is finished there will be a link to this player's stats."));
+					} else {
+						sender.sendMessage(CoreUtils.prefixes("friends") + CoreUtils.colorize(
+								"You are not friends with that player. Follow someone on your forum account, and if you both have linked your Mystic Account and Forum accounts (/register) you will be their friend."));
+					}
+				} else {
+					sender.sendMessage(
+							CoreUtils.prefixes("friends") + CoreUtils.colorize("We couldn't find that player."));
+				}
+
+//				sender.sendMessage("" + CoreUtils.LookupForumID(args[0]));
+//
+//				sender.sendMessage("" + CoreUtils.getMysticPlayer((Player) sender).isFriends(args[0]));
+			}
+
+		} else {
+			sender.sendMessage("Players only. ;)");
 		}
 		return true;
 	}
