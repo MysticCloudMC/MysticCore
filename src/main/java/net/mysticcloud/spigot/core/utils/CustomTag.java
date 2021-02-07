07e80f226c1ede4cec5909d4bebe6cd1941c0888
Cameron-Witcher <58893489+Cameron-Witcher@users.noreply.github.com>
2021-02-07 15:41:12 -0600

    haha ok

--- src/main/java/net/mysticcloud/spigot/core/utils/CustomTag.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/CustomTag.java
@@ -1,7 +1,7 @@
 package net.mysticcloud.spigot.core.utils;
 
 public enum CustomTag {
-	GOD("&e[GOD] "), TEST("&e[Tester] "), NONE("you shouldn't have this.");
+	GOD("&e[GOD] "), TEST("&e[Tester] "), BEAST("&6[BEAST]"), NONE("you shouldn't have this.");
 
 	String tag;
 
