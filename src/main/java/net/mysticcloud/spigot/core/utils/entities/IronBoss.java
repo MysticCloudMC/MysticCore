38a6e2127270f08569ca7feb791e197e1c0c94ed
Cameron-Witcher <CameronWitcher1@gmail.com>
2021-05-16 13:32:56 -0500

    sdsd

--- src/main/java/net/mysticcloud/spigot/core/utils/entities/IronBoss.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/entities/IronBoss.java
@@ -93,7 +93,7 @@ public class IronBoss extends EntityIronGolem {
 	public void movementTick() {
 		super.movementTick();
 		format.setLifetime(z);
-		armor.teleport(getBukkitEntity().getLocation().clone().add(0,2,0));
+		armor.teleport(getBukkitEntity().getLocation().clone().add(0,1.5,0));
 		format.display(new Location(Bukkit.getWorld(world.getWorld().getName()), locX(), locY(), locZ()));
 
 		try {
