b0b5bcda53a0dab5bf55fbdfd2a633aabea82ca9
Cameron-Witcher <CameronWitcher1@gmail.com>
2021-06-08 17:36:05 -0500

    page stuff

--- src/main/java/net/mysticcloud/spigot/core/utils/GUIManager.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/GUIManager.java
@@ -265,8 +265,8 @@ public class GUIManager {
 		inv.addItem(new ItemStack(Material.BLACK_STAINED_GLASS_PANE), "&7Choose an option.", 'X', (String[]) null);
 		inv.addItem(new ItemStack(Material.BARRIER), "&cRemove Tag", 'Y', (String[]) null);
 
-		inv.addItem(new ItemStack(Material.BARRIER), "&aNext Page", 'n', (String[]) null);
-		inv.addItem(new ItemStack(Material.BARRIER), "&aPrevious Page", 'p', (String[]) null);
+		inv.addItem(new ItemStack(Material.BARRIER), "&aPage &7" + (page + 1), 'n', (String[]) null);
+		inv.addItem(new ItemStack(Material.BARRIER), "&aPage &7" + (page - 1), 'p', (String[]) null);
 
 		c.add('X');
 		c.add('X');
