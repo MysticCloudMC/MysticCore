aa7030b94f5aed5f0e97e2939a1943aa04c34b00
Cameron-Witcher <CameronWitcher1@gmail.com>
2021-06-26 22:39:58 -0500

    lol ok

--- src/main/java/net/mysticcloud/spigot/core/utils/placeholder/PlaceholderUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/placeholder/PlaceholderUtils.java
@@ -28,7 +28,8 @@ public class PlaceholderUtils {
 		string = string.replaceAll("%pl", player.getName());
 		string = string.replaceAll("%world", player.getWorld().getName());
 		string = string.replaceAll("%time", CoreUtils.getTime());
-		string = string.replaceAll("%balance", "" + mp.getBalance());
+		string = string.replaceAll("%balance", "%bal");
+		string = string.replaceAll("%bal", "" + mp.getBalance());
 		string = string.replaceAll("%gems", "" + mp.getGems());
 		string = string.replaceAll("%g", "" + mp.getGems());
 		string = string.replaceAll("%level", "" + mp.getLevel());
@@ -41,7 +42,7 @@ public class PlaceholderUtils {
 		string = string.replaceAll("%fulltime", CoreUtils.getTime());
 		string = string.replaceAll("%playertime", player.getPlayerTime() + "");
 		string = string.replaceAll("%suffix", CoreUtils.colorize(CoreUtils.getPlayerSuffix(player)));
-		string = string.replaceAll("%server", Bukkit.getName());
+		string = string.replaceAll("%server", CoreUtils.getServerName());
 
 		if (string.contains("%tag"))
 			string = string.replaceAll("%tag",
