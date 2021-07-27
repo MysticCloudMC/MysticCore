2d33644624b59cd26be3d4c90c8658d6b0476e9b
Cameron-Witcher <CameronWitcher1@gmail.com>
2021-07-26 20:11:30 -0500

    ok

--- src/main/java/net/mysticcloud/spigot/core/utils/CoreUtils.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/CoreUtils.java
@@ -183,8 +183,8 @@ public class CoreUtils {
 		}
 		try {
 			connected = true;
-			db = new IDatabase(SQLDriver.MYSQL, "sql.mysticcloud.net", "Minecraft", 3306, "admin", "v4pob8LW");
-			fdb = new IDatabase(SQLDriver.MYSQL, "sql.mysticcloud.net", "Forums", 3306, "admin", "v4pob8LW");
+			db = new IDatabase(SQLDriver.MYSQL, "lewis.mysticcloud.net", "Minecraft", 3306, "blue", "g3&clly");
+			fdb = new IDatabase(SQLDriver.MYSQL, "lewis.mysticcloud.net", "Forums", 3306, "blue", "g3&clly");
 			if (db.init() && fdb.init())
 				Bukkit.getConsoleSender().sendMessage(prefixes.get("sql") + "Successfully connected to MySQL.");
 		} catch (NullPointerException | SQLException ex) {
